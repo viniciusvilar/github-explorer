5e71784ccaca9d96eeb4d6f8cbfad4e1a4a59920
viniciusvilar <61420617+viniciusvilar@users.noreply.github.com>
2022-12-07 12:10:56 -0300

    Configurando typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -4,8 +4,14 @@ import { useState, useEffect } from "react";
 
 // https://api.github.com/orgs/rocketseat/repos
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos').then(response => response.json()).then(data => setRepositories(data))
